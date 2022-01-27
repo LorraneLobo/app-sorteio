23655f1a4b75de197fa3814961b1ccf0cf3c7a2d
Lorrane Alves <Lorrane.dev@gmail.com>
2022-01-27 09:58:02 -0300

    Finalizado

--- app/src/main/java/com/lorrane/appsorteio/MainActivity.kt
+++ app/src/main/java/com/lorrane/appsorteio/MainActivity.kt
@@ -2,10 +2,19 @@ package com.lorrane.appsorteio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sortearNumero(view: View){
+        var texto = findViewById<TextView>(R.id.textSorteio)
+        var numeroSorteado = Random.Default.nextInt(11)
+        texto.text = "Número sorteado: " + numeroSorteado
     }
 }
